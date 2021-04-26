ceb25ba41c7467dedfa57647a1bf638fbddb8a09
trini-tA <frederic.gacon@free.fr>
2021-04-26 10:40:19 +0200

    Install and configure bootstrap 4.x

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,4 @@ import '../css/app.scss';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore :)');
+require('bootstrap');
